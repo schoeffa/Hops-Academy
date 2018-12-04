f8b38f3e424b3005af4e053cf8c9fad4fd4c82c8
aaron <schoeffa@gmail.com>
2018-12-04 17:12:23 -0500

    Reworked game component and added next round functionality

--- public/app/game/game.js
+++ public/app/game/game.js
@@ -1,9 +1,49 @@
 "use strict";
 const game = {
-    template: `<section class="game-wrapper"><stats user="$ctrl.user"></stats><section class="right-pane"><beer-list></beer-list><trivia></trivia></section></section>`,
-    controller: ["TriviaService", function(TriviaService) {
+    template: `<section class="game-wrapper">
+    <stats user="$ctrl.user" round-num="$ctrl.roundNum"></stats>
+    <section class="right-pane">
+    <beer-list current-beers="$ctrl.currentBeers"></beer-list>
+    <trivia evaluate="$ctrl.evaluate()" show-correct="$ctrl.showCorrect" show-wrong="$ctrl.showWrong" new-round="$ctrl.newRound()" current-question="$ctrl.currentQuestion"></trivia>
+    </section>
+    </section>`,
+    controller: ["TriviaService", function (TriviaService) {
         const vm = this;
         vm.user = TriviaService.getUser();
+        vm.roundNum = 0;
+        vm.showCorrect = false;
+        vm.showWrong = false;
+
+        vm.newRound = () => {
+            vm.roundNum++;
+            vm.search();
+            vm.beers();
+            vm.showCorrect = false;
+        }
+
+        vm.search = () => {
+            TriviaService.getTrivia(vm.user.difficulty).then((result) => {
+                vm.currentQuestion = result;
+                console.log(vm.currentQuestion);
+
+            })
+        }
+
+        vm.beers = () => {
+            TriviaService.getBeer().then((result) => {
+                for (let entry of result) {
+                    if (!entry.abv) {
+                        entry.abv = 4.5;
+                    }
+                }
+                vm.currentBeers = result;
+            });
+        }
+
+
+
+        vm.beers();
+        vm.search();
     }]
 };
 
