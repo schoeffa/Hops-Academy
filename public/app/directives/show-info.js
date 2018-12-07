59856fa63933f0f6bc157cc23e42dd17e71bd8d6
aaron <schoeffa@gmail.com>
2018-12-07 11:05:53 -0500

    Fixed clickability issue on beer

--- public/app/directives/show-info.js
+++ public/app/directives/show-info.js
@@ -6,7 +6,7 @@ function showInfo() {
         link: function($scope, $element, $attrs) {
             $element.on("click", () => {
                 $element[0].children[1].children[1].style.display = "flex";
-
+                $element["0"].style.pointerEvents = "none";
             });
         }
     }
